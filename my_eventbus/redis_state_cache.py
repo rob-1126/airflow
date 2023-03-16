60609d751c8363feebb9af02750618ea44c08c0d
Rob J. Caskey <rob.caskey@astronomer.io>
2023-03-16 10:53:49 -0400

    mild clean-up

--- my_eventbus/redis_state_cache.py
+++ my_eventbus/redis_state_cache.py
@@ -1,24 +1,44 @@
-from airflow.utils.state_cache import BaseStateCacheBackend
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from __future__ import annotations
 
-# imports for our poc state backend
-import tempfile
+import asyncio
+import json
 import pathlib
-from queue import Queue, Empty
-from threading import Thread
-from os import getpid, path
-import pytz
 
 # EventBus = resolve_event_bus_backend()
 # event_bus = event_bus()
-
 import socket
-import asyncio
-import json
+
+# imports for our poc state backend
+import tempfile
 from datetime import datetime
+from os import getpid, path
+from queue import Empty, Queue
+from threading import Thread
+
+import pytz
+
+from airflow.utils.state_cache import BaseStateCacheBackend
 
 existing_client = False
 
 tz = pytz.timezone("UTC")
+
 
 class StateCache(BaseStateCacheBackend):
     def __init__(self):
@@ -38,24 +58,19 @@ class StateCache(BaseStateCacheBackend):
             client = self.get_sync_client(state_backend_client_config)
             client.connect()
             existing_client = client
-            print("initialized new init_state_cache_client")
             return existing_client
         else:
             raise Exception("State cache client can only be initialized once.")
 
     def start(self):
         """This runs within a thread started by the cli"""
-        print("Starting asyncio temporary state backend")
-        print("Connecting to event bus")
         key_vals = {}
         key_vals["favorite_color"] = "blue"
 
         async def handle_invalidate_dag_cache():
             key_vals["universal_last_updated"] = tz.localize(datetime.utcnow()).isoformat()
-            print("Invalidating dag cache.")
 
         async def _get_key(key):
-            print(f"Server retrieving value for {key}")
             val = key_vals[key]
             return val.encode("utf-8")
 
@@ -84,7 +99,7 @@ class StateCache(BaseStateCacheBackend):
                     writer.write(result)
                     writer.write(b"\r\n")
                     result = await _get_key(key)
-                    data = result.encode('utf-8')
+                    data = result.encode("utf-8")
                     writer.write(data)
                     writer.write(b"\r\n")
                 elif cmd == "get_dag_last_updated":
@@ -94,17 +109,12 @@ class StateCache(BaseStateCacheBackend):
                         result = key_vals["universal_last_updated"]
                     else:
                         result = None
-                    data = json.dumps({ "value": result })
+                    data = json.dumps({"value": result})
                     writer.write(data.encode("utf-8"))
                     writer.write(b"\r\n")
                 else:
                     await reader.readline()
                     raise UnknownCommandFromTempStateClient(cmd)
-                #await writer.drain()
-                #print("Drained")
-                # print("Closing conn")
-                # writer.close()
-                # await writer.wait_closed()
 
         async def main_cache_server_loop():
             from airflow.utils.eventbus import resolve_event_bus_backend
@@ -113,18 +123,14 @@ class StateCache(BaseStateCacheBackend):
             event_bus = EventBusBackend()
             await event_bus.connect()
             message = await event_bus.subscribe("secondary")
-            print(f"Starting a socket server on {self.socket_filename}")
             server = await asyncio.start_unix_server(
                 client_connected_cb=client_connected_cb, path=self.socket_filename
             )
             serve_cache_clients = server.serve_forever()
             while True:
-                print("Awaiting message")
                 message = await event_bus.get_message("secondary")
-                print("Got message", message, type(message))
                 data = message["data"]
                 payload = json.loads(data)
-                print("GOT OBJ", payload)
                 op = payload["op"]
                 if op == "invalidate_dag_cache":
                     await handle_invalidate_dag_cache()
@@ -132,14 +138,8 @@ class StateCache(BaseStateCacheBackend):
 
         asyncio.run(main_cache_server_loop())
 
-        print(f"listening on {self.socket_filename}")
-        backlog = 20
-
-        print(f"Starting socket on {self.socket_filename}")
-
     def set_client_config(self, client_config):
         self.client_config = client_config
-        print("Set client config to", client_config)
 
     def make_client_config(self):
         """clients can and will usually be local forked processes"""
@@ -163,38 +163,29 @@ class TemporaryStateBackendClient:
     def __init__(self, client_config):
         self.client_config = client_config
         self.local_event_queue = Queue()
-        print("Created new state backend client for", self.client_config)
-        print("ZAR PREPARING TO RUN")
         self.run()
 
     def run_sync(self, dag_bag):
         """runs inside the remote thread and processes events from the message queue"""
         # todo prune obsolete events from queue
         local_event_queue = self.local_event_queue
-        print("RUNNING REQUEST FOR ", local_event_queue)
         while True:
             try:
                 item = local_event_queue.get(block=False)
-                print("FOUND SOMETHING")
             except Empty:
-                print("WAS EMPTY")
                 break
-            print("ZAR Got item", item, getpid())
 
             if item == "invalidate_dagbag":
-                print("ZAR INVALIDATING", getpid())
                 dag_bag.invalidate_cache()
                 local_event_queue.task_done()
 
     def run(self):
         import time
 
-        socket_path = self.client_config["socket_path"]
+        self.client_config["socket_path"]
 
         def run_thread(local_event_queue):
-            print("ZAR RUNNING thread")
             while True:
-                print("ZAR SUBMITTING INVALIDATE CB")
                 local_event_queue.put("invalidate_dagbag")
                 time.sleep(15)
 
@@ -211,29 +202,16 @@ class UnknownCommandFromTempStateClient(Exception):
 class TemporarySyncStateBackendClient:
     def __init__(self, client_config):
         self.client_config = client_config
-        print("Created new state backend client for", self.client_config)
-        print("ZAR PREPARING TO RUN")
 
     def _read_line(self):
-        #read_buffer = bytearray()
         return self.socket.makefile().readline()
-        #while True:
-        #    chunk = self.socket.recv(4096)
-        #    read_buffer.extend(chunk)
-        #    if len(read_buffer) > 1:
-        #        last_two_chars = read_buffer[-2:]
-        #        if last_two_chars == b"\r\n":
-        #            break
-        return read_buffer
 
     def connect(self):
         socket_path = self.client_config["socket_path"]
         self.socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        print(f"Waiting for socket at {socket_path}.")
         connected = False
         while not connected:
             try:
-                print(f"Connecting to {socket_path}")
                 self.socket.connect(socket_path)
                 connected = True
             except ConnectionRefusedError:
@@ -246,7 +224,6 @@ class TemporarySyncStateBackendClient:
         self.socket.send(b"get ")
         self.socket.send(encoded_key_name)
         self.socket.send(b"\r\n")
-        print("SENDING GET")
         response = self._read_line().strip()
         return response
 
@@ -256,39 +233,10 @@ class TemporarySyncStateBackendClient:
         self.socket.send(encoded_dag_id)
         self.socket.send(b"\r\n")
         response = self._read_line().strip()
-        print("GETTING RAW",repr(response),type(response))
         response_data = json.loads(response)
-        print("GOT RESPONSE", response_data)
 
         raw_value = response_data["value"]
         if raw_value is None:
-          return None
+            return None
         else:
-          return datetime.fromisoformat(raw_value)
-
-    def frob(self):
-        print("FROBBING")
-
-
-# quick test
-if __name__ == "__main__":
-    import time
-
-    StateBackend = StateCache
-    state_backend = StateBackend()
-    print("Starting server")
-    server_thread = Thread(target=state_backend.start, args=(), daemon=True)
-    server_thread.start()
-    print("Server started")
-    client_config = state_backend.make_client_config()
-    sync_state_client = state_backend.get_sync_client(client_config)
-    sync_state_client.connect()
-    last_updated = sync_state_client.get_dag_last_updated("my_dag")
-    print("GOT A LAST UPDATE", last_updated)
-    favorite_color = sync_state_client.get_key("favorite_color")
-    print("1. Got favorite color", repr(favorite_color))
-    favorite_color = sync_state_client.get_key("favorite_color")
-    print("2. Got favorite color", repr(favorite_color))
-    time.sleep(5)
-    # state_backend.connect()
-    # print(state_backend.get_client_config())
+            return datetime.fromisoformat(raw_value)
